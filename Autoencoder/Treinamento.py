b0c7e4df7f84463a6661136a2b1efa10b9faeafb
alexv <ugarte_alex@discente.ufg.br>
2023-01-21 01:05:31 -0300

    Estrutura pronta, falta fazer funcionar

--- Autoencoder/Treinamento.py
+++ Autoencoder/Treinamento.py
@@ -1,6 +1,5 @@
 import torch
-from torch.utils.data import DataLoader
-from sklearn.model_selection import KFold
+from torch.utils.data import DataLoader, random_split
 # meus codigos
 from LoaderDados import DadosAE
 from Rede import Encoder, Decoder
@@ -33,56 +32,51 @@ decoder = decoder.cuda()
 loss_fn = loss_fn.cuda()
 
 # Carregue seus dados de treinamento
-train_data = DadosAE()
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+train_data = DadosAE(target_sample_rate=16000)
 
-# Crie o objeto KFold
-kf = KFold(n_splits=kfold, shuffle=True)
+train_size = int(0.8 * len(train_data))  # dividindo em treino e validação
+val_size = len(train_data) - train_size
+train_dataset, val_dataset = random_split(train_data, [train_size, val_size])
+
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
 
 # Armazenando as losses para verificar o overfitting
 train_losses = []
 val_losses = []
 
 # Loop de treinamento
-for train_index, val_index in kf.split(train_data):
-    # Separe seus dados de treinamento e validação
-    train_subset = train_data[train_index]
-    val_subset = train_data[val_index]
+# Loop de épocas
+for epoch in range(num_epochs):
+    # Treine o modelo com o subset de treinamento
+    encoder.train()
+    decoder.train()
 
-    # Crie DataLoaders para o subset de treinamento e validação
-    train_subset_loader = DataLoader(train_subset, batch_size=64, shuffle=True)
-    val_subset_loader = DataLoader(val_subset, batch_size=64, shuffle=False)
+    train_loss = 0
+    for input, target in train_loader:
+        optimizer.zero_grad()
+        encoded_Data = encoder(input)
+        decoded_Data = decoder(encoded_Data)
+        loss = loss_fn(decoded_Data, target)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+    train_loss /= len(train_loader)
+    train_losses.append(train_loss)
 
-    # Loop de épocas
-    for epoch in range(num_epochs):
-        # Treine o modelo com o subset de treinamento
-        encoder.train()
-        decoder.train()
-
-        train_loss = 0
-        for input, target in train_subset_loader:
-            optimizer.zero_grad()
+    # Avalie o modelo com o subset de validação
+    encoder.eval()
+    decoder.eval()
+    with torch.no_grad():
+        val_loss = 0
+        for input, target in val_loader:
             encoded_Data = encoder(input)
             decoded_Data = decoder(encoded_Data)
-            loss = loss_fn(decoded_Data, target)
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.item()
-        train_loss /= len(train_subset_loader)
-        train_losses.append(train_loss)
-
-        # Avalie o modelo com o subset de validação
-        encoder.eval()
-        decoder.eval()
-        with torch.no_grad():
-            val_loss = 0
-            for input, target in val_subset_loader:
-                encoded_Data = encoder(input)
-                decoded_Data = decoder(encoded_Data)
-                val_loss += loss_fn(decoded_Data, target).item()
-            val_loss /= len(val_subset_loader)
-            val_losses.append(val_loss)
-            print(f'Epoch {epoch}, Validation Loss: {val_loss}')
+            val_loss += loss_fn(decoded_Data, target).item()
+        val_loss /= len(val_loader)
+        val_losses.append(val_loss)
+        print(f'Epoch {epoch}, Validation Loss: {val_loss}')
 
 # Salve o modelo
 torch.save(encoder.state_dict(), 'encoder.pth')
